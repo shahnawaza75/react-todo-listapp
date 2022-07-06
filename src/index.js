43513f7ec8bf5e3d0eee2e6f4afc32fb76673d0f
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-07-06 11:18:14 +0530

    Added files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Component file
+import TodoContainer from './components/TodoContainer';
+
+// stylesheet
+import './App.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// strict mode enable checks and warning not only for the component but also its descendants
